fce1c5c3449177dd96bf1ba755d48871543ca3d5
SamSamCork <70600466+SamSamCork@users.noreply.github.com>
2023-08-28 14:16:33 -0400

    Update script.js
    added custom slider and first edition of on/off button js code

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+// Slider Code Below
 const slider = document.getElementById('slider');
 const sliderVal = document.getElementById('sliderVal');
 
 slider.addEventListener("input", event => {
   const sliderValue = slider.value;
-  const roundedValue = Math.round(sliderValue / 5) * 5
   sliderVal.textContent = sliderValue;
+  slider.style.setProperty('--sliderOpac', sliderValue / 100 + 0.2)
 });
 slider.addEventListener("mouseup", event => {
   const sliderValue = slider.value;
@@ -19,3 +20,25 @@ slider.addEventListener("mouseup", event => {
   }
   sliderVal.textContent = roundedValue;
 });
+
+// Button Code Below
+const button = document.getElementById('onoff');
+
+button.addEventListener("mouseup", event => {
+  // Send signal to the ESP32
+  sendESP('on')
+});
+
+
+// Functions Below
+function sendESP(command) {
+  // Will need to edit with correct address & API info
+  fetch(`http://esp32_ip_address/control?command=${command}`)
+    .then(response => response.text())
+    .then(data => {
+      console.log('Received response from ESP32:', data);
+    })
+    .catch(error => {
+      console.error('Error sending command:', error);
+    });
+}
